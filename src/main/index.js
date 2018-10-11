39a062f6a957b3591b956bc89c189c111595cbcb
JaimeVelasco <contact@jaimevelas.co>
2018-10-11 17:19:42 -0500

    add edit menu to app

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron'
 
 /**
  * Set `__static` path to static files in production
@@ -33,6 +33,29 @@ function createWindow () {
 }
 
 app.on('ready', createWindow)
+
+app.on('ready', () => {
+  // Create the Application's main menu
+  var template = [{
+    label: 'Application',
+    submenu: [
+      { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+      { type: 'separator' },
+      { label: 'Quit', accelerator: 'Command+Q', click: function () { app.quit() } }
+    ] }, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+    ] }
+  ]
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
