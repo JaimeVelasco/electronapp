9cd12ffdcebeb57705f172125c630f4310d4a21d
JaimeVelasco <contact@jaimevelas.co>
2018-10-11 15:13:29 -0500

    add count methods

--- src/renderer/components/CustomerIdForm.vue
+++ src/renderer/components/CustomerIdForm.vue
@@ -1,57 +1,66 @@
 <template>
-  <section class="subscription">
-    <div class="columns is-centered is-flex-desktop">
-      <div class="column is-4">
-        <h1 class="title has-text-centered">Subscriptions</h1>
-        <div class="field">
-          <p class="control has-icons-left">
-            <input v-model="subid" v-on:keyup.enter="subsHandler" class="input" type="text" placeholder="Enter Stripe Customer ID">
-            <span v-if="valid === 'valid'">
-              <span class="icon is-small is-left">
-                <i class="fas fa-smile"></i>
-              </span>
-            </span>
-            <span v-if="valid === 'invalid'">
-              <span class="icon is-small is-left">
-                <i class="fas fa-frown"></i>
-              </span>
-            </span>
-          </p>
+  <div class="container is-flex-tablet level">
+    <section class="subscription levelitem">
+      <div class="box">
+        <div class="columns is-centered">
+          <div class="column has-text-centered is-7">
+            <img class="logo" src="~@/assets/logo.png" alt="mtrx logo" width="200px">
+            <h1 class="subtitle has-text-centered">Enter Stripe's CustomerID to get the company info</h1>
+          </div>
         </div>
-        <div class="field">
-          <p class="control">
-            <button class="button is-success is-pulled-right" @click="subsHandler">
-              Submit
-            </button>
-          </p>
+        <div class="columns is-centered">
+          <div class="column is-6">
+            <div class="field">
+              <p class="control has-icons-left">
+                <input v-model="subid" v-on:keyup.enter="subsHandler" class="input" type="text" placeholder="Enter Stripe Customer ID" autofocus>
+                <span v-if="valid === 'valid'">
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-smile"></i>
+                  </span>
+                </span>
+                <span v-if="valid === 'invalid'">
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-frown"></i>
+                  </span>
+                </span>
+              </p>
+            </div>
+            <div class="field">
+              <p class="control">
+                <button class="button is-success is-pulled-right" @click="subsHandler">
+                  Submit
+                </button>
+                <span v-if="valid === 'invalid'">
+                  <p class="subtitle">Invalid Customer Id</p>
+                </span>
+                <span v-if="valid === 'searching'">
+                  <p class="subtitle">Waiting for response</p>
+                </span>
+                <span v-if="valid === 'duplicates'">
+                  <p class="subtitle">This Customer ID has more than one company associated with it</p>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="columns is-centered is-flex-desktop">
+          <div class="column is-6">
+            <div v-if="valid === 'valid'">
+              <p class="title is-4">{{company.name}}</p>
+              <p>Free Trial Signup Date: <span class="title is-6">{{dateCreated}}</span></p>
+              <p>Date Last Used: <span class="title is-6">{{lastTimeUsed}}</span></p>
+              <p>Total estimates ever created: <span class="title is-6">{{estimatesTotal}}</span></p>
+              <p>Number of estimates created in last 7 days: <span class="title is-6"> {{sevenDayEstimatesTotal}}</span></p>
+              <p>Most recent estimate created on: <span class="title is-6"> {{mostRecentEstimate}}</span></p>
+              <p>Total number of invoices ever created: <span class="title is-6">{{invoicesTotal}}</span></p>
+              <p>Number of Invoices created in last 7 days: <span class="title is-6">{{sevenDayInvoicesTotal}}</span></p>
+              <p>Most recent invoice created on: <span class="title is-6"> {{mostRecentInvoice}}</span></p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="columns is-centered is-flex-desktop">
-      <div class="column is-3">
-        <div v-if="valid === 'invalid'" class="info">
-          <p class="subtitle">Invalid Subscription Id.</p>
-        </div>
-        <div v-if="valid === 'searching'" class="info">
-          <p class="subtitle">looking for subs...</p>
-        </div>
-        <div v-if="valid === 'duplicates'" class="info">
-          <p class="subtitle">This subscription has more than one company associated with it.</p>
-        </div>
-        <div v-if="valid === 'valid'">
-          <h1 class="title info">{{company.stripe && company.stripe.customerID}}</h1>
-          <p>Company Name: <span class="title is-5">{{company.name}}</span></p>
-          <p>Stripe Subscription ID: <span class="title is-5">{{company.stripe && company.stripe.subscriptionID}}</span></p>
-          <p>Free Trial Signup Date: <span class="title is-5">{{dateCreated}}</span></p>
-          <p>Date Last Used: <span class="title is-5">{{lastTimeUsed}}</span></p>
-          <p>Total estimates ever created: <span class="title is-5">{{estimatesTotal}}</span></p>
-          <p>Number of estimates created in last 7 days: <span class="title is-5"> {{sevenDayEstimatesTotal}}</span></p>
-          <p>Total number of invoices ever created: <span class="title is-5">{{invoicesTotal}}</span></p>
-          <p>Number of Invoices created in last 7 days: <span class="title is-5">{{sevenDayInvoicesTotal}}</span></p>
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -73,11 +82,13 @@ export default {
       sevenDayEstimatesTotal: '',
       invoicesTotal: '',
       sevenDayInvoicesTotal: '',
-      lastTimeUsed: ''
+      lastTimeUsed: '',
+      mostRecentEstimate: '',
+      mostRecentInvoice: ''
     }
   },
   created: function () {
-    console.log('created')
+    // console.log('created')
   },
   methods: {
     subsHandler () {
@@ -89,12 +100,11 @@ export default {
           console.log('error ' + err)
         })
         .then(function (sub) {
-          console.log('hello')
-          if (sub.length === 0) { context.valid = 'invalid' }
-          if (sub.length > 1) { context.valid = 'duplicates' }
+          if (sub.length === 0) return context.valid = 'invalid'
+          if (sub.length > 1) return context.valid = 'duplicates'
           context.valid = 'valid'
           context.company = sub[0].toObject()
-          context.dateCreated = format(new Date(context.company.dateCreated), 'DD/MM/YYYY')
+          context.dateCreated = format(new Date(context.company.dateCreated), 'MMMM Do YYYY')
           Estimates.find({ 'company': context.company._id }).count(function (err, count) {
             if (err) console.log(err)
             context.estimatesTotal = count
@@ -112,10 +122,20 @@ export default {
             if (err) console.log(err)
             context.invoicesTotal = count
           })
+          Invoices.find({ 'company': context.company._id })
+            .sort({ createdAt: 1 })
+            .limit(1).then(invoices => {
+              context.mostRecentInvoice = format(new Date(invoices[0].dateCreated), 'MMMM Do YYYY')
+            })
+          Estimates.find({ 'company': context.company._id })
+            .sort({ createdAt: 1 })
+            .limit(1).then(estimates => {
+              context.mostRecentEstimate = format(new Date(estimates[0].dateCreated), 'MMMM Do YYYY')
+            })
           Sessions.find({ 'company': context.company._id })
             .sort({ createdAt: 1 })
             .limit(1).then(sessions => {
-              context.lastTimeUsed = format(new Date(sessions[0].dateCreated), 'DD/MM/YYYY')
+              context.lastTimeUsed = format(new Date(sessions[0].dateCreated), 'MMMM Do YYYY')
             })
           Invoices.find({
             'company': context.company._id,
@@ -132,12 +152,6 @@ export default {
 }
 </script>
 
-<style scoped>
-  /* .info {
-    margin-top: 5rem;
-  } */
-</style>
-
 <style>
   @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
@@ -147,77 +161,24 @@ export default {
     padding: 0;
   }
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
-
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
   }
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
-
-  main {
+  .logo {
+    margin: 1rem;
     display: flex;
-    justify-content: space-between;
+    align-items: center;
   }
 
-  main > div { flex-basis: 50%; }
-
-  .left-side {
+  .aligner {
     display: flex;
-    flex-direction: column;
+
   }
 
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
+  .subscription {
+    margin: 1rem auto;
+    width: 80vw;
+    min-height: 50vh;
   }
 </style>
